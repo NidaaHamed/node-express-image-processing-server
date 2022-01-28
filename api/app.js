3192e3b524dfb1652f8fd43bd1f40be8f98eb41f
nidaa <nidaasallam@gmail.com>
2022-01-28 15:02:55 +0200

    fix bug declair path

--- api/app.js
+++ api/app.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const app = express()
+const path = require('path')
 
 const pathToIndex = path.resolve(__dirname, '../client/index.html')
 app.use('/*', function(req, res){
