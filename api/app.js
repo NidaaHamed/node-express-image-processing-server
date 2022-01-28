cf30ac6ded5696a22f5c735f52fd5114fe8cfb72
nidaa <nidaasallam@gmail.com>
2022-01-28 18:32:48 +0200

    module-2 completed

--- api/app.js
+++ api/app.js
@@ -3,7 +3,15 @@ const app = express()
 const path = require('path')
 
 const pathToIndex = path.resolve(__dirname, '../client/index.html')
+const router = require('./src/router')
+
+app.use('/', router)
+
+app.use(express.static(path.resolve(__dirname, 'uploads')))
+
 app.use('/*', function(req, res){
   res.sendFile(pathToIndex)
 })
+
+
 module.exports = app
---
+++ api/src/router.js
@@ -0,0 +1,27 @@
+const express = require('express')
+const multer = require('multer')
+const storage = multer.diskStorage({destination: 'api/uploads/', filename: filename})
+const router = express.Router()
+
+const upload = multer({fileFilter:fileFilter, storage: storage})
+
+function filename(req, file, callback) {
+  callback(null, file.originalname)
+}
+
+function fileFilter(req, file, callback) {
+  if(file.mimetype !== 'image/png'){
+    req.fileValidationError = 'Wrong file type'
+    callback(null, false, new Error('Wrong file type'))
+    
+  }else{
+    callback(null, true)
+  }
+}
+
+router.post('/upload', upload.single('photo'), (req, res) => {
+  if(req.fileValidationError) return res.status(400).json({error:req.fileValidationError})
+  else return res.status(201).json({success: true})
+})
+
+module.exports = router
