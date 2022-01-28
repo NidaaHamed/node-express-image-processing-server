539da570369c16bf3a985f5858e7b251130b37cc
nidaa <nidaasallam@gmail.com>
2022-01-28 14:56:00 +0200

    module-1 completed

---
+++ api/app.js
@@ -0,0 +1,8 @@
+const express = require('express')
+const app = express()
+
+const pathToIndex = path.resolve(__dirname, '../client/index.html')
+app.use('/*', function(req, res){
+  res.sendFile(pathToIndex)
+})
+module.exports = app
